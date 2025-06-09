3e38dcda5b6912ee8214b1612b357b25d94f810b
ayazicioglu <aahmetyazicioglu@gmail.com>
2025-06-09 14:23:04 +0200

    pytorch and hf

--- Loading Models.py
+++
@@ -1,5 +0,0 @@
-^# torch for deep learning and neural network modeling.
-!pip install torch
-
-# transformers for accessing pretrained models and performing various NLP tasks with ease.
-!pip install transformers
